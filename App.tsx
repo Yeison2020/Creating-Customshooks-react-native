69be0a0340ddff759c068efa1b269359a025293c
Yeison <enmanuelquiterios@gmail.com>
2022-02-10 11:57:50 -0500

    Added container styles

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import {View, Text} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Text style={{fontSize: 45, textAlign: 'center'}}>Hello World</Text>
     </View>
   );
 };
