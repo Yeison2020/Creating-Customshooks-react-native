3c2f463dd3a54d64c52b330ecac8184b6ebee2c4
Yeison <enmanuelquiterios@gmail.com>
2022-02-13 13:43:55 -0500

    Added another btn botton left

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -7,10 +7,17 @@ const CounterScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>Contador: {counter}</Text>
       <TouchableOpacity
-        style={styles.fab}
+        style={styles.fabLocationBL}
         onPress={() => setCounter(curr => curr + 1)}>
         <View style={styles.fab}>
           <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.fabLocationR}
+        onPress={() => setCounter(curr => curr - 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -48,17 +55,29 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     padding: 15,
   },
+  fabLocationR: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25,
+  },
+
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
   fab: {
     backgroundColor: '#5856D6',
     width: 60,
     height: 60,
     borderRadius: 100,
+    justifyContent: 'center',
   },
 
   fabText: {
     color: 'white',
     fontSize: 25,
-    textAlign: 'center',
     fontWeight: 'bold',
+    alignSelf: 'center',
   },
 });
