48a16e2cbff6fb4ba4c75e7453d446f13fe23759
Yeison <enmanuelquiterios@gmail.com>
2022-02-12 22:37:23 -0500

    Adding styles here

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -26,6 +26,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    backgroundColor: '#707382',
   },
   title: {
     textAlign: 'center',
@@ -33,6 +34,7 @@ const styles = StyleSheet.create({
     top: -15,
   },
   btn2: {
+    height: 70,
     borderRadius: 5,
     marginBottom: 10,
     padding: 15,
