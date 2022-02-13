aabf0b02f8eb34a10db8403b4db0d6eba2047b4a
Yeison <enmanuelquiterios@gmail.com>
2022-02-13 15:12:35 -0500

    Started working with box object model

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {SafeAreaView} from 'react-native';
 // import HolaMundoScreen from './src/screens/HolaMundoScreen';
-import CounterScreen from './src/screens/CounterScreen';
+// import CounterScreen from './src/screens/CounterScreen';
+import BoxObjectModel from './src/screens/BoxObjectModelScreen';
 
 const App = () => {
-  return <CounterScreen />;
+  return (
+    <SafeAreaView>
+      <BoxObjectModel />
+    </SafeAreaView>
+  );
 };
 
 export default App;
---
+++ src/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const BoxObjectModel = () => {
+  return (
+    <View>
+      <Text>BoxObjectModel</Text>
+    </View>
+  );
+};
+
+export default BoxObjectModel;
